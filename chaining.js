b8d4643fad42b5800fe024a7ffc2c8fda808e5b4
Bri Wyatt <mbriwyatt@gmail.com>
2017-02-06 09:21:29 -0600

    inital commit

---
+++ chaining.js
@@ -0,0 +1,16 @@
+console.log("hello chaining array exercise");
+
+// Instructions: Using one single line of JavaScript code, complete the following tasks on the array of integers below.
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+console.log("integers", integers);
+// sort in decending order (biggest # -> smallest #)      .sort(function(nums){return nums < 19}
+// remove integers greater than 19                        .filter()
+// multiply each [i] * 1.5 then subtract - 1              .map()
+// console.log("sumOfRemainingInts",sumOfRemainingInts);  .reduce()
+
+
+var printInts = integers.sort(function(a, b) { return b - a}).filter(function(nums){return nums < 19}).map(function(nums){return nums = (nums * 1.5) -1}).reduce(function(prev, curr) {return prev + curr})
+
+
+console.log("printInts",printInts);
+
